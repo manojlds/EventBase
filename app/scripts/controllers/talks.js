a62a59814a3c8787a524b7be0142ef5118687fc8
manojlds <manojlds@gmail.com>
2013-08-19 00:13:51 +0530

    Adding links to the talk detail pages

--- app/scripts/controllers/talks.js
+++ app/scripts/controllers/talks.js
@@ -1,10 +1,14 @@
 'use strict';
 
-angular.module('EventBaseApp').controller('TalksCtrl', function ($scope, angularFire) {
+angular.module('EventBaseApp').controller('TalksCtrl', function ($scope, $location, angularFire) {
     var url = "https://eventbase.firebaseio.com/talks";
     
     angularFire(url, $scope, "talks", {}).then(function(){
-
     });
+
+    $scope.talkDetail = function(e, talkId){
+        e.preventDefault();
+        $location.path("/talks/" + talkId);
+    }
 
 });
