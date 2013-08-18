4c2ed27793253b196be127808a0dd82a5851e94d
manojlds <manojlds@gmail.com>
2013-08-18 19:35:33 +0530

    Simple authentication with github

--- app/scripts/controllers/propose.js
+++ app/scripts/controllers/propose.js
@@ -1,13 +1,29 @@
 'use strict';
 
 angular.module('EventBaseApp')
-  .controller('ProposeCtrl', function ($scope, $location, angularFire) {
+  .controller('ProposeCtrl', function ($scope, $location, angularFire, angularFireAuth) {
     var url = "https://eventbase.firebaseio.com/talks";
+    
+    angularFireAuth.initialize(url, {scope: $scope, name: "user"});
+    
     angularFire(url, $scope, "talks").then(function() {
-        
+
         $scope.addTalk = function() {
+            if(!$scope.user){
+                alert('You must be logged in to propose a talk');
+                return;
+            }
+
             $scope.talks.push($scope.newTalk);
             $location.path('/');
+        };
+
+        $scope.login = function() {
+            angularFireAuth.login("github");
+        };
+
+        $scope.logout = function() {
+            angularFireAuth.logout();
         };
 
     });
