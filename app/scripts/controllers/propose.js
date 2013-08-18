a83ff079a0ca7ad16ca5fc7001b58247b8861e02
manojlds <manojlds@gmail.com>
2013-08-18 20:05:55 +0530

    Using explicit sync than implicit

--- app/scripts/controllers/propose.js
+++ app/scripts/controllers/propose.js
@@ -1,31 +1,31 @@
 'use strict';
 
-angular.module('EventBaseApp')
-  .controller('ProposeCtrl', function ($scope, $location, angularFire, angularFireAuth) {
+angular.module('EventBaseApp').controller('ProposeCtrl', function ($scope, $location, angularFireCollection, angularFireAuth) {
     var url = "https://eventbase.firebaseio.com/talks";
     
     angularFireAuth.initialize(url, {scope: $scope, name: "user"});
+
+    var ref = new Firebase(url);
+    $scope.talks = angularFireCollection(ref.limit(10));
+
+    $scope.addTalk = function() {
+        if(!$scope.user){
+            alert('You must be logged in to propose a talk');
+            return;
+        }
+        $scope.newTalk.id = $scope.user.id;
+        $scope.newTalk.email = $scope.user.email;
+
+        $scope.talks.add($scope.newTalk);
+        $location.path('/');
+    };
+
+    $scope.login = function() {
+        angularFireAuth.login("github");
+    };
+
+    $scope.logout = function() {
+        angularFireAuth.logout();
+    };
     
-    angularFire(url, $scope, "talks").then(function() {
-
-        $scope.addTalk = function() {
-            if(!$scope.user){
-                alert('You must be logged in to propose a talk');
-                return;
-            }
-
-            $scope.talks.push($scope.newTalk);
-            $location.path('/');
-        };
-
-        $scope.login = function() {
-            angularFireAuth.login("github");
-        };
-
-        $scope.logout = function() {
-            angularFireAuth.logout();
-        };
-
-    });
-    
-  });
+});
